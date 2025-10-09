31c46210b2b4154951f343e6bb6c8c841d4ac7d6
hadv <hadv@miichisoft.net>
2025-10-09 19:59:18 +0700

    update env fe

--- front-end/src/utils/api.ts
+++ front-end/src/utils/api.ts
@@ -1,7 +1,9 @@
 import axios, { type InternalAxiosRequestConfig } from 'axios'
 
+const baseURL = (import.meta as any)?.env?.VITE_API_BASE_URL || 'http://localhost:8081/api'
+
 const api = axios.create({
-  baseURL: 'http://localhost:8081/api',
+  baseURL,
 })
 
 api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
